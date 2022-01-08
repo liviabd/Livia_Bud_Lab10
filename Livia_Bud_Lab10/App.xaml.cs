6bd488670fb0e6d2dc13275637b96a225a0a1b4d
liviabd <liviabd84@gmail.com>
2022-01-08 21:04:50 +0200

    update

--- Livia_Bud_Lab10/App.xaml.cs
+++ Livia_Bud_Lab10/App.xaml.cs
@@ -1,15 +1,29 @@
-﻿using System;
+﻿using Livia_Bud_Lab10.Data;
+using System;
+using System.IO;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace Livia_Bud_Lab10
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                   database = new ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
-
+            MainPage = new NavigationPage(new ListEntryPage());
             MainPage = new MainPage();
         }
 
